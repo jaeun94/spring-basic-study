34fc40fee182594bdab05111857d0457ff388d34
jaeun <p.jaeun94@gmail.com>
2021-10-04 23:48:26 +0900

    빈 생명주기 콜백

---
+++ src/test/java/hello/core/lifeCycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.lifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    // 스프링 빈 이벤트 라이프사이클
+    // 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello.core");
+            return networkClient;
+        }
+    }
+}
