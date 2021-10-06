5b32f89cc61f2dc59c791c67e6fdff23b0698c4f
jaeun <p.jaeun94@gmail.com>
2021-10-06 12:35:30 +0900

    웹 스코프_request 스코프

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    // myLogger 스코프가 request 이기 때문에 provider 사용, http 요청이 올 때 까지 빈 생성 지연
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
